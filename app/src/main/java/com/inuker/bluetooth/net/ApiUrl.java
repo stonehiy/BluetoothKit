fbc0fa6ac30a4fb83eefc82746f2f8e0ee5d8186
stonehiy <stonehiy@gmail.com>
2019-11-18 10:55:38 +0800

    修改充电盒环境

--- app/src/main/java/com/inuker/bluetooth/net/ApiUrl.java
+++ app/src/main/java/com/inuker/bluetooth/net/ApiUrl.java
@@ -8,9 +8,9 @@ public class ApiUrl {
     //远程充电桩开启
     //api/ChargingData/ChargAskStart
     // (string epilecode_in, string epilepoint_in, string controlType_in, string limitdata_in, string eusercardNo_in, string fixTime_in = "00000000")
-    public static final String API_CHARGE_START = BASE_URL + "api/ChargingData/ChargAskStart";
+    public static final String API_CHARGE_START = BASE_URL_DEV + "api/ChargingData/ChargAskStart";
     /// 远程充电桩关闭
     //api/ChargingData/EpileClose(string rid, string membercode)
-    public static final String API_CHARGE_STOP = BASE_URL + "api/ChargingData/EpileClose";
+    public static final String API_CHARGE_STOP = BASE_URL_DEV + "api/ChargingData/EpileClose";
 
 }
